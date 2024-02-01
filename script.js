9604c61ed39702568ba49ffbd601939ce57d839e
inclined4X <jezemiahsam48@gmail.com>
2024-02-01 23:31:23 +0000

    Placed the days in a week in an array

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let dateContainer = document.querySelector(".date-container");
 let hoursContainer = document.querySelector(".hours");
 let minutesContainer = document.querySelector(".minutes");
 let secondsContainer = document.querySelector(".seconds");
+
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const monthNames = [];
