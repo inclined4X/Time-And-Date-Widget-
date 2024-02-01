b5bd5b5eb79a99c349fbf17cce507092d5b07e91
inclined4X <jezemiahsam48@gmail.com>
2024-02-01 23:27:05 +0000

    getting the various classes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Getting the classes
+let dateContainer = document.querySelector(".date-container");
+let hoursContainer = document.querySelector(".hours");
+let minutesContainer = document.querySelector(".minutes");
+let secondsContainer = document.querySelector(".seconds");
